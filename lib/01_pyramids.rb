20fdea5c70ce567a7018982e6aabdf669166e960
SedTheCursed <sed.robert@gmail.com>
2019-04-04 23:12:59 +0200

    Ajout d'une recursivité des fonctions de 01_pyramids.rb afin que chacune produise une pyramide.

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,6 +1,6 @@
 def half_pyramid
 	puts ""
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+	puts "Salut, bienvenue dans ma pyramide montante ! Combien d'étages veux-tu ?"
 	puts "(Rentre un nombre entre 1 et 25)"
 	print "> "	
 	user_number = gets.chomp.to_i
@@ -11,7 +11,7 @@ def half_pyramid
 	brick = 1
 	space = user_number
 
-	if (user_number > 0) && (user_number < 25) 
+	if (user_number > 0) && (user_number <= 25) 
 		
 
 		user_number.times do 
@@ -23,54 +23,60 @@ def half_pyramid
 
 	else
 		puts "Oups, ça n'a pas fonctionné, as-tu bien choisi un nombre entre 1 et 25 ?"
+		half_pyramid
 	end
 
 end
 
-
-
-
-
- 
- def full_pyramid
- 	puts ""
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu (choisis un nombre impair) ?"
-	puts "(Rentre un nombre entre 1 et 25)"
-	print "> "	
-	user_number = gets.chomp.to_i
-
+def top_pyramid(stairs)
 	puts "Voici la pyramide :"
-
 	brick = 1
-	space = user_number - 1
+	space = stairs - 1
 
-	if (user_number > 0) && (user_number%2 != 0) 
-
-		user_number.times do 
-
-			puts (" " * space) + ("#" * brick)
-			brick += 2
-			space -= 1 
-		end
-	else
-		puts "Oups, ça n'a pas fonctionné, as-tu bien choisi un nombre impair ?"
+	stairs.times do 
+		puts (" " * space) + ("#" * brick)
+		brick += 2
+		space -= 1 
 	end
 
 	return brick - 4
 end
+ 
+ def full_pyramid
+ 	puts ""
+	puts "Salut, bienvenue dans ma pyramide triangulaire ! Combien d'étages veux-tu ?"
+	puts "(Rentre un nombre entre 1 et 25)"
+	print "> "	
+	user_number = gets.chomp.to_i
+
+	if (user_number > 0) && (user_number <= 25)
+		top_pyramid(user_number)
+	else
+		puts "Oups, ça n'a pas fonctionné, as-tu bien choisi un nombre entre 1 et 25 ?"
+		full_pyramid
+	end
+end
 
 
+def wtf_pyramid
+	puts ""
+	puts "Salut, bienvenue dans ma pyramide en losange ! Combien d'étages veux-tu ?"
+	puts "(Rentre un nombre impair)"
+	print "> "	
+	user_number = gets.chomp.to_i
 
+	if (user_number > 0) && (user_number % 2 != 0)
+		brick = top_pyramid(user_number)
+		space = 1
 
-
-def wtf_pyramid 
-	brick = full_pyramid
-	space = 1
-
-	while brick > 0 
-		puts (" " * space) + ("#" * brick)
-		brick -= 2
-		space += 1
+		while brick > 0 
+			puts (" " * space) + ("#" * brick)
+			brick -= 2
+			space += 1
+		end
+	else
+		puts "Oups, ça n'a pas fonctionné, as-tu bien choisi un nombre impair ?"
+		wtf_pyramid
 	end
 end
 
