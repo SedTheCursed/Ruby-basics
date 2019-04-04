40e64fbba1c8b65c9d924c70847e14187147b46f
SedTheCursed <sed.robert@gmail.com>
2019-04-04 10:59:52 +0200

    '00_hello.rb' done.

---
+++ lib/00_hello.rb
@@ -0,0 +1,15 @@
+def say_hello(first_name)
+    puts "Bonjour, #{first_name} !"
+end
+
+def ask_first_name
+    puts "Quel est ton prénom ?"
+    print "> "
+    return first_name = gets.chomp
+end
+
+def perform
+    say_hello(ask_first_name)
+end
+
+perform
