f58e8b64f2e4147b23ced893f8df494e38ade31f
SedTheCursed <sed.robert@gmail.com>
2019-04-04 17:46:32 +0200

    Ajout d'un choix de mode de jeu Suppression des messages dans le mode de génération de statistiques Ajout d'une possibilité de rejouer

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,60 +1,75 @@
-def throwDice(stair)
+def throwDice(stair, solo_game = true)
 
 	dice = rand(1..6)
 
-	 puts "Le dé affiche : #{dice}"
+	puts "Le dé affiche : #{dice}" if solo_game
 	
 	if dice >= 5
 		stair += 1 
-		puts "Tu gravis une marche, bravo !"
+		puts "Tu gravis une marche, bravo !" if solo_game
 		
 	
 	elsif dice == 1
 		if stair != 0
 			stair -= 1
-			puts "Tu descends d'une marche."
+			puts "Tu descends d'une marche." if solo_game
 		else
-			puts "Tu es dejà au rez-de-chaussée" 
+			puts "Tu es dejà au rez-de-chaussée" if solo_game
 		end
 
 	else 
-		puts "Tu reste sur la même marche." 
+		puts "Tu reste sur la même marche." if solo_game
 		
 	end
 	
-	puts "Maintenant tu es à la marche n°#{stair}"
-	puts " "
+	puts "Maintenant tu es à la marche n°#{stair}" if solo_game
+	puts " " if solo_game
 
 	return stair
 end
 
-meuh
+def gameMode
+	mode = 0
 
-def game(manual = true)
+	while mode <1 || mode> 3
+		puts "Choisissez votre mode de jeu"
+		puts "  1 : Manuel"
+		puts "  2 : Automatique"
+		puts "  3 : Generation de statistiques"
+		print "> "
+		mode = gets.chomp.to_i
+	end
+
+	return mode
+end
+
+def game(mode)
 	stair = 0
 	turn = 0
 
-	if manual
-		while stair < 10
+	while stair < 10
+		if mode == 3
+			stair = throwDice(stair, false)
+		else
 			stair = throwDice(stair)
-			turn += 1
-			puts "Relance le dé"
-			gets
 		end
-	else
-		while stair < 10
-			stair = throwDice(stair)
-			turn += 1
+
+		turn += 1
+
+		if mode == 1
+			puts "Relance le dé"
+			gets	
 		end
 	end
 
-	puts " "
-	puts "Et voilà ! Tu est arrivé en haut de l'escalier !!! Enjoy the view."
-	puts "Ca t'as pris #{turn} lancés de dé."
+	unless mode == 3
+		puts " "
+		puts "Et voilà ! Tu est arrivé en haut de l'escalier !!! Enjoy the view."
+		puts "Ca t'as pris #{turn} lancés de dé."
+	end
 
 	return turn
 end
-
 
 
 def average_finish_time
@@ -65,7 +80,7 @@ def average_finish_time
 	puts "Simulation de parties"
 
 	rand(100..255).times do
-		sum += game(false)
+		sum += game(3)
 		nb_of_games += 1
 	end
 	
@@ -73,5 +88,22 @@ def average_finish_time
 
 end
 
-game #Partie manuelle
-average_finish_time #simulation avec des parties automatiques
+def process
+	mode = gameMode
+
+	if mode == 3
+		average_finish_time
+	else
+		game(mode)
+	end
+
+	puts "Veux-tu refaire une partie ? (Oui/Non)"
+	print "> "
+	replay = gets.chomp
+
+	if replay == "Oui"
+		process
+	end
+end
+
+process
